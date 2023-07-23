96b828319504ee0431f61a229610fd40843d18c7
Manuel Roa <manuel.roaojeda23@gmail.com>
2023-07-23 17:34:57 -0300

    api-go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,16 +1,27 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type ResponseGenerico struct {
+	Estado  string
+	Mensaje string
+}
+
 func Ejemplo_get(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(response, "ejemplo get")
+	response.Header().Set("Content-type", "application/json")
+	output, _ := json.Marshal(ResponseGenerico{"ok", "GET"})
+	fmt.Fprintln(response, string(output))
 }
 
 func Ejemplo_get_id(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(response, "ejemplo get_id")
+	vars := mux.Vars(request)
+	fmt.Fprintln(response, "ejemplo get_id"+vars["id"])
 }
 
 func Ejemplo_post(response http.ResponseWriter, request *http.Request) {
@@ -18,9 +29,12 @@ func Ejemplo_post(response http.ResponseWriter, request *http.Request) {
 }
 
 func Ejemplo_delete(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(response, "ejemplo delete")
+	vars := mux.Vars(request)
+	fmt.Fprintln(response, "ejemplo delete"+vars["id"])
+
 }
 
 func Ejemplo_put(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(response, "ejemplo put")
+	vars := mux.Vars(request)
+	fmt.Fprintln(response, "ejemplo put"+vars["id"])
 }
--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 	mux := mux.NewRouter()
 	prefijo := "/api/v1/"
 	mux.HandleFunc(prefijo+"ejemplo", handlers.Ejemplo_get).Methods("GET")
-	mux.HandleFunc(prefijo+"ejemplo", handlers.Ejemplo_get_id).Methods("GET")
+	mux.HandleFunc(prefijo+"ejemplo/{id:[0-9]+}", handlers.Ejemplo_get_id).Methods("GET")
 	mux.HandleFunc(prefijo+"ejemplo", handlers.Ejemplo_post).Methods("POST")
-	mux.HandleFunc(prefijo+"ejemplo", handlers.Ejemplo_put).Methods("PUT")
-	mux.HandleFunc(prefijo+"ejemplo", handlers.Ejemplo_delete).Methods("DELETE")
+	mux.HandleFunc(prefijo+"ejemplo/{id:[0-9]+}", handlers.Ejemplo_put).Methods("PUT")
+	mux.HandleFunc(prefijo+"ejemplo/{id:[0-9]+}", handlers.Ejemplo_delete).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8081", mux))
 
